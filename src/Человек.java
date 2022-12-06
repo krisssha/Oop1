a4d8d89c5dec19797816f0cef290e325e12fee32
Кристина <duleba@eml.ru>
2022-12-06 20:31:19 +0700

    первые два задания ооп Человек

--- src/Человек.java
+++ src/Человек.java
@@ -40,4 +40,26 @@ public class Человек {
         return "Привет! Меня зовут " + getName() + "! Я из города " + getTown() + "! Я родился в " +
                 getYearOfBirth() + " году." + " Я работаю на должности " + getJobTitle() + ". Будем знакомы!";
     }
+
+    public Человек(String name, String town, int yearOfBirth, String jobTitle) {
+        if (name == null || name.equals("")) {
+            name = "Информация не указана";
+        }
+        this.name = name;
+
+        if (town == null || town.equals("")) {
+            town = "Информация не указана";
+        }
+        this.town = town;
+
+        if (yearOfBirth >= 0) {
+            this.yearOfBirth = yearOfBirth;
+        } else {
+            this.yearOfBirth = 0;
+        }
+        if (jobTitle == null || jobTitle.equals("")) {
+            jobTitle = "Информация не указана"; }
+            this.jobTitle = jobTitle;
+
+    }
 }
